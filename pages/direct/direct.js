eec63cb96e7fa616501e082c70c95467b7fd3f69
fyonecon <fyonecon@gmail.com>
2025-05-08 17:59:20 +0800

    1

--- pages/direct/direct.js
+++ pages/direct/direct.js
@@ -1,3 +1,9 @@
 function start_page(info) {
-
+    let dom = $(".div-box");
+    let domain = window.location.host;
+    if (view.string_include_string(domain, ".github.io") >=0){
+        dom.removeClass("hide");
+    }else{
+        window.location.replace("./?route=home");
+    }
 }
