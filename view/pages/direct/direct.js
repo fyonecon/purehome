284c18839d4de21ffcc5a6c109dc300cfe390baf
fyonecon <fyonecon@gmail.com>
2025-05-12 13:20:37 +0800

    迁移网站到更短的网址

--- view/pages/direct/direct.js
+++
@@ -1,9 +0,0 @@
-function start_page(info) {
-    let dom = $(".div-box");
-    let domain = window.location.host;
-    if (view.string_include_string(domain, ".github.io") >=0){
-        dom.removeClass("hide");
-    }else{
-        window.location.replace("./?route=home");
-    }
-}
